20acc650a6c1aab336b7b79f2fe854c939364822
maria helena <130762954+marilenaz@users.noreply.github.com>
2024-04-19 09:47:50 -0300

    test

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual os melhor time?");
+    var respostaTime = prompt("Qual a melhor marca?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "Adidas") {
+      alert("Isso mesmo! Adidas é a melhor marca!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
